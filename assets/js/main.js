c6796895d8523b7082e63cd6dd5773b3d09d9689
Pawan <pawan.144prg@gmail.com>
2021-08-20 19:25:36 +0530

    about page added

---
+++ assets/js/main.js
@@ -0,0 +1,11 @@
+var typed = new Typed('#typed', {
+    stringsElement: '#typed-strings',
+    typeSpeed: 50,
+    smartBackspace: true,
+    backDelay: 700,
+    backspeed: 30,
+    loop: true,
+    loopCount: Infinity, 
+    
+
+  });
